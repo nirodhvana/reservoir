b2f207b5ed5244153fd3294b4d9470b9cb7d596f
Saatwat <105187044+nishkaamprem@users.noreply.github.com>
2024-08-08 18:15:02 +0545

    some scripts and changes

--- .config/wal/templates/colors-wal-dwm.h
+++ .config/wal/templates/colors-wal-dwm.h
@@ -4,11 +4,11 @@ static const char norm_border[] = "{color8}";
 
 static const char sel_fg[] = "{color15}";
 static const char sel_bg[] = "{color2}";
-static const char sel_border[] = "{color15}";
+static const char sel_border[] = "{color1}";
 
 static const char urg_fg[] = "{color15}";
 static const char urg_bg[] = "{color1}";
-static const char urg_border[] = "{color1}";
+static const char urg_border[] = "{color15}";
 
 static const char *colors[][3]      = {{
     /*               fg           bg         border                         */
